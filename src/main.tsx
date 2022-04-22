26c52daa94257c30fa87bba252dfa06609314d63
Vinicius Vicentini <viniciusvicentini@live.com>
2022-04-21 23:28:53 -0300

    feat: add redux-persist

--- src/main.tsx
+++ src/main.tsx
@@ -3,17 +3,20 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/lib/integration/react'
 
 import { App } from './App'
 
-import { store } from './store'
+import { persistor, store } from './store'
 
 import './styles/global.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
 )
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,14 +1,27 @@
 import { configureStore } from '@reduxjs/toolkit'
 
+import { persistStore, persistReducer } from 'redux-persist'
+
+import storage from 'redux-persist/lib/storage'
+
 import thunk from 'redux-thunk'
 
 import reducers from './reducers'
 
+const persistConfig = {
+  key: 'ADVENTURE',
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, reducers)
+
 export const store = configureStore({
-  reducer: reducers,
+  reducer: persistedReducer,
   devTools: process.env.NODE_ENV !== 'production',
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk),
 })
+
+export const persistor = persistStore(store)
 
 export type RootState = ReturnType<typeof store.getState>
 
